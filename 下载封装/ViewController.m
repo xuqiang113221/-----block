d5dc155b70333c5dafc3bf30d9dcadd6efd2e529
xuqiang <495219869@qq.com>
2013-10-15 10:54:59 +0800

    Initial Commit

---
+++ 下载封装/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  下载封装
+//
+//  Created by xuqiang on 13-10-15.
+//  Copyright (c) 2013年 xuqiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
