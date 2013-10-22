11b98dcd8ca5737634fd660867f28c2fe926caaf
xuqiang <495219869@qq.com>
2013-10-22 09:50:36 +0800

    1
    123

---
+++ 下载封装/SubViewController.m
@@ -0,0 +1,39 @@
+//
+//  SubViewController.m
+//  下载封装
+//
+//  Created by xuqiang on 13-10-22.
+//  Copyright (c) 2013年 xuqiang. All rights reserved.
+//
+
+#import "SubViewController.h"
+
+@interface SubViewController ()
+
+@end
+
+@implementation SubViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    //看看有没有
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
